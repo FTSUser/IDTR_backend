e64dbde986cd393c54bd2101592863d9943c839c
parag <dev020.rejoice@gmail.com>
2022-03-09 18:34:41 +0530

    issue fixed

--- src/_metronic/_partials/dashboards/Demo1Dashboard.js
+++ src/_metronic/_partials/dashboards/Demo1Dashboard.js
@@ -13,6 +13,8 @@ import {
 // import { ApiPost } from "../../../helpers/API/ApiData";
 export function Demo1Dashboard() {
   const [count, setCount] = useState();
+  const [examinercount, setExaminerCount] = useState();
+  const [dataenteryrcount, setDataEnteryCount] = useState();
   let userInfo = getUserInfo();
 
 
@@ -26,29 +28,58 @@ export function Demo1Dashboard() {
       })
       .catch((err) => { });
   }
+  const getAllDataEntry = async () => {
+    const data = {
+      Examiner: userInfo?.admin[0]?._id,
+    };
+    await ApiGet(
+      `batch/getBatchByDataEntry/${data?.Examiner}?page=1&limit=10`
+    )
+      .then((res) => {
+
+        setDataEnteryCount(res?.data?.payload?.count);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  const getAllExaminer = async () => {
+    const data = {
+      Examiner: userInfo?.admin[0]?._id,
+    };
+    await ApiGet(
+      `batch/getBatchByExaminer/${data?.Examiner}?page=1&limit=10`
+    )
+      .then((res) => {
+
+        setExaminerCount(res?.data?.payload?.count);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
 
   useEffect(() => {
     console.log("userInfo", userInfo);
-    if (userInfo?.admin[0]?.role?.roleName === 'superadmin') {
+    if (userInfo?.admin[0]?.role?.roleName === 'superadmin' || userInfo?.admin[0]?.role?.roleName === 'admin') {
       getAll()
+    }
+    if (userInfo?.admin[0]?.role?.roleName === 'Data Entry') {
+      getAllDataEntry()
+    }
+    if (userInfo?.admin[0]?.role?.roleName === 'Examiner') {
+      getAllExaminer()
     }
   })
   return (
     <>
-      {
 
-        userInfo?.admin[0]?.role?.roleName != 'superadmin'  &&
-        <div className="row" >
-          <div className="col-lg-12 col-xxl-12 p-0">
-            <MixedWidget1 className="card-stretch gutter-b" />
-          </div>
-        </div>
-      }
 
 
       {
         userInfo?.admin[0]?.role?.roleName === 'superadmin' &&
-
         <div className="row align-items-center">
           <div className="col-lg-4 col-xxl-4 mb-5 ">
             <div className="card card-custom py-5">
@@ -135,6 +166,131 @@ export function Demo1Dashboard() {
             </div>
           </div>
 
+        </div>
+      }
+      {
+        userInfo?.admin[0]?.role?.roleName === 'admin' &&
+        <div className="row align-items-center">
+          <div className="col-lg-4 col-xxl-4 mb-5 ">
+            <div className="card card-custom py-5">
+              <div className="card-body p-0 text-center">
+                <div className="">
+                  <div className="mb-3"><h1>User</h1></div>
+                  <div>
+                    <h3>{count?.Users}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-xxl-4 mb-5">
+            <div className="card card-custom py-5">
+              <div className="card-body p-0 text-center">
+                <div className="">
+                  <div className="mb-3"><h1>Vehicle Category</h1></div>
+                  <div>
+                    <h3>{count?.vehicleCategory}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-xxl-4 mb-5">
+            <div className="card card-custom py-5">
+              <div className="card-body p-0 text-center">
+                <div className="">
+                  <div className="mb-3"><h1>Course Category</h1></div>
+                  <div>
+                    <h3>{count?.courseCategory}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-xxl-4 mb-5">
+            <div className="card card-custom py-5">
+              <div className="card-body p-0 text-center">
+                <div className="">
+                  <div className="mb-3"><h1>Course Name</h1></div>
+                  <div>
+                    <h3>{count?.courseName}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-xxl-4 mb-5">
+            <div className="card card-custom py-5">
+              <div className="card-body p-0 text-center">
+                <div className="">
+                  <div className="mb-3"><h1>Course Type</h1></div>
+                  <div>
+                    <h3>{count?.courseType}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-xxl-4 mb-5">
+            <div className="card card-custom py-5">
+              <div className="card-body p-0 text-center">
+                <div className="">
+                  <div className="mb-3"><h1>Data Entery</h1></div>
+                  <div>
+                    <h3>{count?.dataEntrys}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-xxl-4 mb-5">
+            <div className="card card-custom py-5">
+              <div className="card-body p-0 text-center">
+                <div className="">
+                  <div className="mb-3"><h1>Examiner</h1></div>
+                  <div>
+                    <h3>{count?.examiners}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      }
+
+      {
+        userInfo?.admin[0]?.role?.roleName === 'Data Entry' &&
+        <div className="row align-items-center">
+          <div className="col-lg-4 col-xxl-4 mb-5 ">
+            <div className="card card-custom py-5">
+              <div className="card-body p-0 text-center">
+                <div className="">
+                  <div className="mb-3"><h1>Batch List</h1></div>
+                  <div>
+                    <h3>{dataenteryrcount ? dataenteryrcount : 0}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+      {
+        userInfo?.admin[0]?.role?.roleName === 'Examiner' &&
+        <div className="row align-items-center">
+          <div className="col-lg-4 col-xxl-4 mb-5 ">
+            <div className="card card-custom py-5">
+              <div className="card-body p-0 text-center">
+                <div className="">
+                  <div className="mb-3"><h1>Batch List</h1></div>
+                  <div>
+                    <h3>{examinercount ? examinercount : 0}</h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       }
     </>
